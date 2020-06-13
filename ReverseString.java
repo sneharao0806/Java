866c48a3841927bb3e7221ff998a8513ed7b3f93
sneha madhavaram <42950216+sneharao0806@users.noreply.github.com>
2020-06-13 14:16:44 -0500

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -6,11 +6,18 @@ public class ReverseString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        
+				//declaring string objects
+
 		 String str="java Demo";
 		 String revstr=" ";
+		
+				//get length of the input string 
+
 		 
 		 int len=str.length();
-		 
+		
+		 		//code to reverse string 
+
 		 
 		 for(int i=len-1;i>=0;i--) {
 			 revstr= revstr+ str.charAt(i);
