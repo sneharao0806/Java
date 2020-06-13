10c05c333ba9aa4c3becfaefc520720b351b992c
sneha madhavaram <42950216+sneharao0806@users.noreply.github.com>
2020-06-13 14:13:15 -0500

    Work on Reverse String

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+package Strings;
+
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		 String str="java Demo";
+		 String revstr=" ";
+		 
+		 int len=str.length();
+		 
+		 
+		 for(int i=len-1;i>=0;i--) {
+			 revstr= revstr+ str.charAt(i);
+		 }
+
+			 System.out.println("Reverse of the string"+revstr);
+		 
+		
+	}
+
+}
