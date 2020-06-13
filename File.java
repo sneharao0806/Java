71426314a9b1a1876863550a11dd8726d20b5f06
sneha madhavaram <42950216+sneharao0806@users.noreply.github.com>
2020-06-13 12:01:27 -0500

    Commit

---
+++ File.java
@@ -0,0 +1,45 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class File {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+   Scanner scan=new Scanner(System.in);
+  // PrintWriter s=new PrintWriter("C:\\Users\\sneha\\eclipse-workspace\\OpenFile\\data.txt");
+   
+   
+   
+   
+//   PrintWriter k3 = new PrintWriter("C:\\Users\\Rocke\\OneDrive\\Desktop\\dest.txt");
+//	int num = 0,count=1;
+//	System.out.println("number puudu: ");
+//	while(count<=3) {
+//		try {
+//			int k2 = may.nextInt();
+//			if(k2<0) {
+//				throw new UserException("Cannot have negative numbers bud");
+//				throw new UserException("Cannot have negatives bud:");
+//			}
+//			k3.println(k2);
+//			num=num+k2;
+//			count++;
+//		}
+//		catch(UserException e) {
+//			System.out.println(e.toString());
+//		}
+//	}
+//	System.out.println("sum is: "+num);
+////	k3.print("sum is: "+num);
+//	may.close();
+//	k3.close();
+//	
+//
+//	k1.close();
+//	k.close();
+	}
+
+}
